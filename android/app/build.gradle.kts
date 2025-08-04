2249e70d969694b4ccb3244897d18320f0fa1045
Seungbin Oh <sboh1214@gmail.com>
2025-08-04 21:45:33 +0900

    chore: add google firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "dev.sboh.app_lock_flutter"
+    namespace = "com.applockFlutter"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.missionland_app"
+        applicationId = "dev.sboh.app_lock_flutter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
